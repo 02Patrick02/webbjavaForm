2ac7fab6e541c74ea2bfcbbd6f1bd16c5e414605
02Patrick02 <paku02001@utb.vaxjo.se>
2021-08-24 11:34:05 +0200

    form med javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+let form = document.querySelector("form");
+let object;
+
+form.addEventListener("submit", event => {
+    let namn= form.elements.namn.value;
+    let klass = form.elements.klass.value;
+    object = {
+        "name":namn,
+        "klass":klass
+    };
+
+    console.log(JSON.stringify(object));
+    event.preventDefault();
+});
